04927f7f1079d0b7eb60ac5e8012c8afbf7f2947
junion <junion@junion.org>
2013-11-12 01:19:53 +0900

    first commit

---
+++ content.js
@@ -0,0 +1,16 @@
+// 検索結果にfaviconを追加する
+$(document).bind('DOMNodeInserted', function(event) {
+    // 検索結果のAタグ前にfaviconを追加する
+    $('h3.r>a:first-child').each(function(){
+        // 追加済みでない場合のみ追加する
+        if (!$(this).children('img')[0]) {
+            var domain  = $(this).attr('href').split('/')[2];
+            var favicon = $('<img>').attr({
+                'src': "//www.google.com/s2/favicons?domain=" + domain,
+                'class': 'search-results-with-favicon',
+            });
+            $(this).prepend(favicon);
+        }
+    });
+});
+
