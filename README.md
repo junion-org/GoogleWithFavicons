ec56dcf73a89899a0f55060b1700f9892d365a34
junion <junion@junion.org>
2014-11-03 23:53:42 +0900

    published

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-GoogleWithFavicons
-==================
+Google with Favicons
+====================
+A Chrome extension which shows favicons in Google search results.
 
-Chrome extension that shows favicons in google search results.
+Detailed description
+--------------------
+"Google with Favicons" is a Chrome extension which shows favicons in Google search results.
+You can get more user-friendly and cool search experience.
+
+Screenshot
+----------
+![Google with Favicons](https://github.com/junion-org/GoogleWithFavicons/raw/master/screenshot.png)
--- js/content.js
+++ js/content.js
@@ -1,12 +1,9 @@
-// 検索結果にfaviconを追加する
 var timer = 0;
+var delay = 100;
 $(document).bind('DOMNodeInserted', function(event) {
-    // timerが未初期化の場合は動作しない
     if (timer) return;
-    // 検索結果のAタグ前にfaviconを追加する
-    timer = setTimeout(function(){
-        $('h3.r>a:first-child').each(function(){
-            // 追加済みでない場合のみ追加する
+    timer = setTimeout(function() {
+        $('h3.r>a:first-child').each(function() {
             if (!$(this).children('img')[0]) {
                 var domain  = $(this).attr('href').split('/')[2];
                 var favicon = $('<img>').attr({
@@ -17,6 +14,5 @@ $(document).bind('DOMNodeInserted', function(event) {
             }
         });
         timer = 0;
-    }, 100);
+    }, delay);
 });
-
